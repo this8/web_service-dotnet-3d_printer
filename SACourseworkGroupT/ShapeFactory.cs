754b573dfb3757d8db1afaafa06f445b32d7c780
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2022-11-09 15:38:16 +0530

    ShapeFactory patch

--- SACourseworkGroupT/ShapeFactory.cs
+++ SACourseworkGroupT/ShapeFactory.cs
@@ -4,25 +4,27 @@ using System.Linq;
 using System.Web;
 using SACourseworkGroupT.Shapes;
 
-namespace SACourseworkGroupT
+namespace SACourseworkGroupT.Shapes
 {
     public class ShapeFactory
     {
         public IShape CreateShape(string name, double x, double y, double z)
         {
-            if (name == "Circle")
+            IShape shape = null;
+
+            if (name.ToLower().Equals("circle"))
             {
-                return new Circle(x);
+                shape = new Circle(x);
             }
-            else if (name == "Rectangle")
+            else if (name.ToLower().Equals("rectangle"))
             {
-                return new Rectangle(x, y);
+                shape = new Rectangle(x, y);
             }
-            else if (name == "Triangle")
+            else if (name.ToLower().Equals("triangle"))
             {
-                return new Triangle(x, y, z);
+                shape = new Triangle(x, y, z);
             }
-            return null;
+            return shape;
         }
     }
 }
